24b22c1c5c3a7e9ab994dda187ba8de14f3c8fbd
Jorn van Wier <jornvanwier@gmail.com>
2017-03-23 13:42:13 +0100

    add file routing

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ extern crate serde_json;
 #[macro_use] extern crate serde_derive;
 
 use rocket_contrib::Template;
+use rocket::response::NamedFile;
+use std::path::{Path, PathBuf};
 
 #[derive(Debug, Serialize)]
 struct Page {
@@ -25,6 +27,29 @@ fn greet(name: &str) -> String {
     format!("Hello {}!", name)
 }
 
+// Javascript
+#[get("/js/<file..>")]
+fn javascript_files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("js/").join(file)).ok()
+}
+// CSS
+#[get("/css/<file..>")]
+fn css_files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("css/").join(file)).ok()
+}
+
+// Other files
+#[get("/files/<file..>")]
+fn other_files(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/").join(file)).ok()
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![sup, greet]).launch();
+    rocket::ignite().mount("/", routes![
+        sup, 
+        greet,
+        javascript_files,
+        css_files,
+        other_files
+        ]).launch();
 }
